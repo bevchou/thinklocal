a127b2e8b12b70a189466bad9523bf087f336897
Lin Zhang <lin.zhang.ux@gmail.com>
2020-06-08 15:39:43 -0700

    use useContext for updating loggedin status and user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
@@ -11,40 +11,48 @@ import About from "./pages/About";
 
 import Header from "./components/Header";
 
+import { UserContext } from "./UserContext";
 import "./App.scss";
 
 const history = createBrowserHistory();
 
 function App() {
   const [isLoggedInState, setIsLoggedInState] = useState(false);
-
   const [zipcodeState, setZipcodeState] = useState(null);
+  const [user, setUser] = useState(null)
+
+  const providerValue = useMemo(() => ({ 
+    isLoggedInState, 
+    setIsLoggedInState,
+    user,
+    setUser}), [isLoggedInState, setIsLoggedInState, user, setUser]);
 
   return (
     <>
       <Router history={history} basename="/demo">
-          <Header
-            isLoggedInState={isLoggedInState}
-            setIsLoggedInState={setIsLoggedInState}
-          />
+          <UserContext.Provider value={providerValue}>
+          <Header/>
           <Switch>
             <Route
               exact
               path="/"
               component={() => (
                 <Homepage
-                  isLoggedInState={isLoggedInState}
                   zipcodeState={zipcodeState}
                   setZipcodeState={setZipcodeState}
                 />
               )}
             />
             <Route exact path="/about" component={About} />
-            <Route exact path="/signin" component={SignIn} />
+            <Route 
+              exact 
+              path="/signin" 
+              component={SignIn} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/event/:eventId" component={Event} />
             <Route exact path="/group/:name" component={Group} />
           </Switch>
+          </UserContext.Provider>
       </Router>
     </>
   );
