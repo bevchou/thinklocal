2f2711211be346c00784a5d80ab3ae2f4268a27a
Beverly <bc2542@nyu.edu>
2020-06-10 00:32:51 -0700

    show error message if links are not availabe to share

--- src/components/ShareModal.js
+++ src/components/ShareModal.js
@@ -8,14 +8,18 @@ const ShareModal = ({ linksToShare, setModalState }) => {
   return (
     <div className="modal">
       {/* set modal to null to close out of it */}
-      <div className="exitModal" onClick={() => setModalState(null)}>X</div>
+      <div className="exitModal" onClick={() => setModalState(null)}>
+        X
+      </div>
       <h2>Share this event!</h2>
       <div className="shareButtons">
-        {linksToShare.map((link) => (
-          <button key={link.name} onClick={() => openLink(link.url)}>
-            {link.name}
-          </button>
-        ))}
+        {linksToShare && linksToShare.length > 0
+          ? linksToShare.map((link) => (
+              <button key={link.name} onClick={() => openLink(link.url)}>
+                {link.name}
+              </button>
+            ))
+          : "The organizer of this event has not added any links yet ):"}
       </div>
     </div>
   );
